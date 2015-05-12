8f84942d0188db0cdb42b9918928f34006fbcd40
patryk <pkociubowski@gmail.com>
2015-05-12 09:08:25 +0200

    travis enabled

--- karma.conf.js
+++ karma.conf.js
@@ -1,113 +1,100 @@
 // Karma configuration
 
-var ANGULAR_JS_VERSION ="1_2_16";
+module.exports = function (config) {
+    config.set({
 
-module.exports = function(config) {
-  config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '.',
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '.',
+        // frameworks to use
+        frameworks: ['jasmine'],
 
-    // frameworks to use
-    frameworks: ['jasmine'],
+        // list of files / patterns to load in the browser
+        files: [
+            './node_modules/angular/angular.js',
+            './node_modules/angular-mocks/angular-mocks.js',
+            './node_modules/angular-flash/dist/angular-flash.js',
+            './node_modules/angular-translate/dist/angular-translate.js',
+            './node_modules/ng-i18n/dist/*.js',
 
-    // list of files / patterns to load in the browser
-    files: [
-      // Angular libs
-      './../../test-runner/libs/angularjs/' + ANGULAR_JS_VERSION + '/angular.js',
+            './test/BaseTestHelper.js',
+            './src/main/assets/**/*.coffee',
 
-      // Testing libs
-      './../../test-runner/libs/angularjs/' + ANGULAR_JS_VERSION + '/angular-mocks.js',
+            // Test helper
+            './test/**/*.spec.js',
+            './test/**/*.spec.coffee'
+        ],
 
-      // App libs
-      './../../test-runner/public/javascripts/**/*.js',
+        // list of files to exclude
+        exclude: [
+            './../../test-runner/public/javascripts/**/*.min.js',
+            'target/scala-2.10/resource_managed/main/public/**/*.min.js',
+            'target/scala-2.10/resource_managed/main/public/**/Extensions.*'
+        ],
 
-      // Test helper
-      './../../test-runner/libs/javascript/BaseTestHelper.js',
-      './test/BaseTestHelper.js',
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['junit', 'coverage'],
 
-      // CoffeeScript dependencies
-      './../common-web/src/main/assets/**/*.coffee',
-      './../i18n-web/src/main/assets/**/*.coffee',
+        // web server port
+        port: 9876,
 
-      // CoffeeScript Complied JS
-      './src/main/assets/**/*.coffee',
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-      // Test helper
-      './test/**/*.spec.js',
-      './test/**/*.spec.coffee'
-    ],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
 
-    // list of files to exclude
-    exclude: [
-      './../../test-runner/public/javascripts/**/*.min.js',
-      'target/scala-2.10/resource_managed/main/public/**/*.min.js',
-      'target/scala-2.10/resource_managed/main/public/**/Extensions.*'
-    ],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['junit', 'coverage'],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: ['PhantomJS', 'Chrome'],
 
-    // web server port
-    port: 9876,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+        // JUnit XML Configuration
+        junitReporter: {
+            outputFile: 'target/karma-reports/junit/client-test-results.xml',
+            suite: ''
+        },
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // Source files, that you wanna generate coverage for
+        // Do not include tests or libraries - (these files will be instrumented by Istanbul)
+        preprocessors: {
+            '../common-web/src/main/assets/**/*.coffee': ['coffee'],
+            '../i18n-web/src/main/assets/**/*.coffee': ['coffee'],
+            '**/*.coffee': ['coffee'],
+            './src/main/assets/**/*.coffee': ['coverage']
+        },
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['PhantomJS','Chrome'],
+        // Configure the reporter
+        coverageReporter: {
+            reporters: [
+                {
+                    type: 'html',
+                    dir: 'target/karma-reports/coverage/html/'
+                },
+                {
+                    type: 'cobertura',
+                    dir: 'target/karma-reports/coverage/cobertura/',
+                    file: 'cobertura-client-coverage.xml'
+                }
+            ]
+        }
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true,
-
-    // JUnit XML Configuration
-    junitReporter: {
-      outputFile: 'target/karma-reports/junit/client-test-results.xml',
-      suite: ''
-    },
-
-    // Source files, that you wanna generate coverage for
-    // Do not include tests or libraries - (these files will be instrumented by Istanbul)
-    preprocessors: {
-        '../common-web/src/main/assets/**/*.coffee': ['coffee'],
-        '../i18n-web/src/main/assets/**/*.coffee': ['coffee'],
-        '**/*.coffee': ['coffee'],
-        './src/main/assets/**/*.coffee': ['coverage']
-    },
-
-    // Configure the reporter
-    coverageReporter: {
-      reporters:[
-            {
-                type: 'html',
-                dir: 'target/karma-reports/coverage/html/'
-            },
-            {
-                type: 'cobertura',
-                dir: 'target/karma-reports/coverage/cobertura/',
-                file: 'cobertura-client-coverage.xml'
-            }
-        ]
-    }
-
-  });
+    });
 };
